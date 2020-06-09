2c69fe44f99831aed2ab345cfade953f37fdbed5
pankajjain333@gmail.com <PasswordU2>
2020-06-09 21:31:32 +0530

    model fixes

--- proximity-person-details-service/src/main/kotlin/org/sai/app/model/Models.kt
+++ proximity-person-details-service/src/main/kotlin/org/sai/app/model/Models.kt
@@ -14,7 +14,7 @@ data class PersonDetails(var id: String?,val firstName: String?,val lastName: St
 
     data class SelfAssessmentAnswer(val questionId:String,val answer:String)
     data class SelfAssessmentQuestion(var id: String?,val question: String,val options: List<String>,val mandatory: Boolean=true)
-    data class SelfAssessment(var id: String?,val personId: String,var timestamp: Long?, val questions:List<SelfAssessmentAnswer>)
+    data class SelfAssessment(var id: String?,val personId: String,var timestamp: Long?, val answers:List<SelfAssessmentAnswer>)
 
     interface SelfAssessmentService {
         fun save(selfAssessmentQuestion: SelfAssessmentQuestion): Boolean
